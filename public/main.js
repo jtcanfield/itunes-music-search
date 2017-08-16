65d896b80311cc341aed93e6e3334190a24eb035
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-16 17:40:53 -0400

    add working search

--- public/main.js
+++ public/main.js
@@ -11,7 +11,28 @@ function searchSubmit(){
           return;
         }
         response.json().then(function(data) {
-          console.log(data.results);
+          let resultsArray = data.results;
+          let parentbody = document.querySelector('#searchappender');
+          parentbody.innerHTML = "";
+          resultsArray.map((i) =>{
+            let parentDiv = document.createElement('div');
+            parentbody.appendChild(parentDiv);
+            let backgroundimg = document.createElement('img');
+            backgroundimg.setAttribute("style", "background-image: url("+i.artworkUrl100+"); height: 100px; width: 100px;");
+            let artistname = document.createElement('a');
+            artistname.innerHTML = "<br>Artist: " + i.artistName;
+            artistname.setAttribute("href", i.artistViewUrl);
+            let songname = document.createElement('a');
+            songname.innerHTML = "<br>Track Name: " + i.trackName;
+            songname.setAttribute("href", i.trackViewUrl);
+            let albumname = document.createElement('a');
+            albumname.innerHTML = "<br>Album: " + i.collectionName;
+            albumname.setAttribute("href", i.collectionViewUrl);
+            parentDiv.appendChild(backgroundimg);
+            parentDiv.appendChild(artistname);
+            parentDiv.appendChild(songname);
+            parentDiv.appendChild(albumname);
+          })
         });
       }
     )
