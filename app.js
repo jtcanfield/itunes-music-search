76d46f2f523525765328858381c95ee768ba9c96
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-16 17:03:18 -0400

    add working renderer into fetch

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const mustacheExpress = require('mustache-express');
 const port = 3000;
 const Promise = require('es6-promise').Promise;
+const es6 = require('es6-promise').polyfill();
+const isomorphic = require('isomorphic-fetch');
 app.use(express.static(__dirname + '/public'));
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
@@ -10,10 +12,10 @@ app.set('view engine', 'mustache');
 
 
 
-var input = document.getElementById("textinput");
-input.addEventListener('keypress', function(e) {
-  if (e.keyCode === 13) {searchSubmit();}
-});
+// var input = document.getElementById("textinput");
+// input.addEventListener('keypress', function(e) {
+//   if (e.keyCode === 13) {searchSubmit();}
+// });
 function searchSubmit(){
   fetch("https://itunes.apple.com/search?term=" + "knife")
     .then(
@@ -24,19 +26,19 @@ function searchSubmit(){
         }
         response.json().then(function(data) {
           console.log(data);
+          app.get('/index/', function (req, res) {
+            res.render('index', data);
+          });
         });
       }
     )
     .catch(function(err) {
       console.log("Fetch Error: ", err);
     });
-}searchSubmit()
+}
+searchSubmit()
 
 
-
-app.get('/index/', function (req, res) {
-  res.render('index', data);
-});
 
 app.listen(port, function(){
   console.log("Server active on http://localhost:3000/");
