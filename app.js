70f6c461cf68f4806fe86c28e25c6ff18c11d1c2
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-16 17:01:02 -0400

    add fetch

--- app.js
+++ app.js
@@ -8,6 +8,32 @@ app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
+
+
+var input = document.getElementById("textinput");
+input.addEventListener('keypress', function(e) {
+  if (e.keyCode === 13) {searchSubmit();}
+});
+function searchSubmit(){
+  fetch("https://itunes.apple.com/search?term=" + "knife")
+    .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log(response.status);
+          return;
+        }
+        response.json().then(function(data) {
+          console.log(data);
+        });
+      }
+    )
+    .catch(function(err) {
+      console.log("Fetch Error: ", err);
+    });
+}searchSubmit()
+
+
+
 app.get('/index/', function (req, res) {
   res.render('index', data);
 });
